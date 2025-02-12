4d5d23ac9efe3304d904c9aa2fd0581874782a36
sstrac <s.r.strachan@outlook.com>
2025-02-12 15:59:53 +0000

    break once

--- level/infrastructure/generator.gd
+++ level/infrastructure/generator.gd
@@ -1,10 +1,17 @@
 extends Interactable
 
 @onready var animation_player = get_node("AnimationPlayer")
+@onready var smoke = get_node("Smoke")
+
+
+var broken = false
 
 func _ready():
 	z_index = position.y
 
 
 func interact():
-	animation_player.play('break')
+	if not broken:
+		animation_player.play('break')
+		smoke.visible = false
+		broken = true
