8a3ab03518ffa3ac1b6438b8a9e5bd5bc081c585
sstrac <s.r.strachan@outlook.com>
2025-02-16 16:11:49 +0000

    Fix grammer

--- assets/scene_dialogue/scenes.gd
+++ assets/scene_dialogue/scenes.gd
@@ -16,9 +16,9 @@ const SCENE_1: Dictionary = {
 	'script':
 	[
 		"left:The scanners said this planet was a verdant paradise... what the hell happened?",
-		"right:The scanners were last updated 12,436 years ago, it seems a civilisation grew and collapsed within that timeframe",
-		"left:How could they destroy their own planet in only 12,00 years!",
-		"right:12,436 years. Perhaps we can look around and reverse some of the damages"
+		"right:The scanners were last updated 12,436 years ago, it seems a civilisation grew and collapsed within that timeframe.",
+		"left:How could they destroy their own planet in only 12,000 years!",
+		"right:12,436 years. Perhaps we can look around and reverse some of the damages."
 	]
 }
 
@@ -29,10 +29,10 @@ const SCENE_2: Dictionary = {
 	},
 	'script':
 	[
-		"left:They have pigs on this planet?! They are so cute!",
+		"left:They have pigs on this planet?! They're so cute!",
 		"right:Pigs, the latin 'porcus', is a species native to earth. What could they be doing on this planet?",
-		"left:Are they coughing? They dont look healthy",
-		"right:It seems the atmosphere of this planet is unusually thick with smog, perhaps we could assist",
+		"left:Are they coughing? They dont look healthy.",
+		"right:It seems the atmosphere of this planet is unusually thick with smog, perhaps we could assist."
 		
 	]
 }
@@ -44,7 +44,7 @@ const SCENE_3: Dictionary = {
 	},
 	'script':
 	[
-		"right:Perhaps this pig is searching for a watering hole"
+		"right:Perhaps this pig is searching for a watering hole."
 	]
 }
 
@@ -55,7 +55,7 @@ const SCENE_NEW_ITEM: Dictionary = {
 	},
 	'script':
 	[
-		"right:You seem to have found a new item, switch to it by pressing 'TAB'... It is a button in your spacesuit"
+		"right:You seem to have found a new item, switch to it by pressing 'TAB'... It is a button in your spacesuit."
 	]
 }
 
@@ -66,10 +66,10 @@ const SCENE_LONELY_PIGS: Dictionary = {
 	},
 	'script':
 	[
-		"right:The vital sighs of these pigs seem to be improving",
+		"right:The vital signs of these pigs seem to be improving.",
 		"left:Something still seems off about them.. maybe they're lonely?",
-		"right:Earth pigs have been known to seek out the company of other pigs and often huddle to maintain physical contact",
-		"left:Maybe we can find others somewhere, reunite the family"
+		"right:Earth pigs have been known to seek out the company of other pigs and often huddle to maintain physical contact.",
+		"left:Maybe we can find others somewhere, reunite the family."
 	]
 }
 
@@ -81,7 +81,7 @@ const SCENE_PIG_EVOLUTION: Dictionary = {
 	'script':
 	[
 		"left:There we go, the pigs look so much happier!",
-		"right:I am detecting strange deviations in the pigs melecular structure, perhaps we should stand back",
+		"right:I am detecting strange deviations in the pigs molecular structure, perhaps we should stand back.",
 		"left:WHAT?! Oh. My. God!"
 	]
 }
@@ -96,7 +96,7 @@ const SCENE_PIG_EVOLUTION_FOLLOWUP: Dictionary = {
 		"left:Well I never thought I'd see a pig fly!",
 		"right:...",
 		"left:Nothing to say?",
-		"right:...My databases have no precident for this",
-		"left:Huh, Guess we're both stumped then"
+		"right:...My databases have no precident for this.",
+		"left:Huh, Guess we're both stumped then. Let's head back to the ship."
 	]
 }
